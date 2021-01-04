ab84e8b3dc600316ea3360e78ca8293dd4a38574
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2021-01-04 09:43:22 +0800

    Update bypa.sh

--- padavan/BYPA/bypa.sh
+++ padavan/BYPA/bypa.sh
@@ -77,14 +77,17 @@ EOF
 /sbin/restart_dhcpd
 	fi
 	al_online=`cat /etc/storage/dnsmasq/dnsmasq.conf | grep "3,$BYP_IP4"`	
-	al_exit=`nvram show | grep dhcp_dnsv6_x | grep "$BYP_IP6" | awk -F "=" '{print $2}'`
+	[ -n "$BYP_IP6" ] && al_exit=`nvram show | grep dhcp_dnsv6_x | grep "$BYP_IP6" | awk -F "=" '{print $2}'` || al_exit="1"
 	(time nslookup www.baidu.com $BYP_IP4 ) 2> /tmp/bypa.log
 	time=`cat /tmp/bypa.log | grep real | awk '{print $3}' | awk -F "." '{print $1}'`
-	if [ "$time"x == "0"x ] && [ -z "$al_exit" -a -z "$al_online" ]; then
+	if [ "$time"x == "0"x ]; then
 		[ -z "$al_exit" -o -z "$al_online" ] && add_dhcp
 		exit 0
+	else
+		if [ -n "$al_exit" -a "$al_exit" -ne "1" ] || [ -n "$al_online" ]; then
+			logger -t "【BYPA】" "旁路由下线，开始调整dhcp选项" && del_dhcp
+		fi
 	fi
-	[ -n "$al_exit" -o -n "$al_online" ] && logger -t "【BYPA】" "旁路由下线，开始调整dhcp选项" && del_dhcp
 }
 
 case $1 in
