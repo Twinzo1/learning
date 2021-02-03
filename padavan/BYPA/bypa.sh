1767e202b65c7e884913eecec62527c5043a4632
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2021-02-03 23:05:40 +0800

    Update bypa.sh

--- padavan/BYPA/bypa.sh
+++ padavan/BYPA/bypa.sh
@@ -7,12 +7,11 @@ LOGFILE="/tmp/log/bypa.log"
 # 获取旁路由mac地址，必填(或填ipv4地址)
 BYP_MAC=`nvram get bypa_macaddr_x`
 [ -z "$BYP_MAC" ] && BYP_MAC=""
-
 # 获取旁路由ipv4地址
 BYP_IP4=`nvram get bypa_ipaddr_x`
 [ -z "$BYP_IP4" ] && BYP_IP4="10.0.0.2"
 # 我的旁路由ip
-[ -z "$BYP_IP4" -a -z "$BYP_MAC" ] && logger -t "【BYPA】" "需填写IP或mac地址，脚本退出" && exit 0
+[ -z "$BYP_IP4" -a -z "$BYP_MAC" ] && logger -t "【BYPA】" "需填写旁路由IP或mac地址，脚本退出" && exit 0
 [ -z "$BYP_IP4" ] && BYP_IP4=`cat /proc/net/arp | grep -i "$BYP_MAC" | awk -F " " '{print $1}' 2>/dev/null`
 
 # 通过ipv4地址获取mac
@@ -25,39 +24,33 @@ BYP_IP6=`ip -6 neighbor show | grep -i "$BYP_MAC" | sed -n '1p' | awk -F " " '{p
 BYP_PAC=`nvram get bypa_pac_url`
 [  -z "$BYP_PAC" ] && BYP_PAC="http://10.0.0.2/music.pac"
 
-# 额外设置，比如指定dhcp-option-force
-extra_setting=$(echo `nvram get bypa_extra_setting`)
 # 添加dhcp_option
 add_dhcp()
 {
-	sed -i "/dhcp-option=greatwall,3,$BYP_IP4/d" /etc/storage/dnsmasq/dnsmasq.conf
+	sed -i "/#added by bypa/d" /etc/storage/dnsmasq/dnsmasq.conf
 	# 只指定greatwall的mac网关为旁路由ip，会得不到nat1
 	# 使用dhcp-mac=dhcp-mac=set:greatwall,<MAC address>标记需要科学的mac地址
-	sed -i "/dhcp-option=lan,6,$BYP_IP4/d" /etc/storage/dnsmasq/dnsmasq.conf
-	sed -i "/dhcp-option=lan,252/d" /etc/storage/dnsmasq/dnsmasq.conf
-	nvram set dhcp_dnsv6_x=""
-	cat "dhcp-option=greatwall,3,$BYP_IP4" > /tmp/bypa.conf
-	cat "dhcp-option=lan,6,$BYP_IP4" > /tmp/bypa.conf
-	cat "dhcp-option=lan,252,$BYP_PAC" > /tmp/bypa.conf
-	cat "dhcp-option=greatwall,3,$BYP_IP4" > /tmp/bypa.conf
-cat >> /etc/storage/dnsmasq/dnsmasq.conf << EOF
-dhcp-option=greatwall,3,$BYP_IP4
-dhcp-option=lan,6,$BYP_IP4
-dhcp-option=lan,252,$BYP_PAC
-EOF
-	[ -z "$BYP_PAC" ] && sed -i "/dhcp-option=lan,252/d" /etc/storage/dnsmasq/dnsmasq.conf
+	echo "dhcp-option=greatwall,3,$BYP_IP4" "#added by bypa" > /tmp/bypa.conf
+	echo "dhcp-option=lan,6,$BYP_IP4" "#added by bypa" >> /tmp/bypa.conf
+	[ ! -z "$BYP_PAC" ] && echo "dhcp-option=lan,252,$BYP_PAC" "#added by bypa" >> /tmp/bypa.conf
+	# 额外设置，比如指定dhcp-option-force
+	extra_setting_num=`nvram show | grep "bypa_ex_set_x"`
+	for es in $extra_setting_num
+	do 
+		[ -n `awk -F "=" '{print $2}'` ] && echo $es "#added by bypa" >> /tmp/bypa.conf
+	done
+	cat /tmp/bypa.conf >> /etc/storage/dnsmasq/dnsmasq.conf
   	nvram set dhcp_dnsv6_x="$BYP_IP6"
   	nvram commit
   	/sbin/restart_dhcpd
+	rm /tmp/bypa.conf
 	logger -t "【BYPA】" "旁路由上线，开始调整dhcp选项"
 }
 
 # 删除dhcp_option
 del_dhcp()
 {
-	sed -i "/dhcp-option=greatwall,3,$BYP_IP4/d" /etc/storage/dnsmasq/dnsmasq.conf
-	sed -i "/dhcp-option=lan,6,$BYP_IP4/d" /etc/storage/dnsmasq/dnsmasq.conf
-	sed -i "/dhcp-option=lan,252/d" /etc/storage/dnsmasq/dnsmasq.conf
+	sed -i "/#added by bypa/d" /etc/storage/dnsmasq/dnsmasq.conf
 	nvram set dhcp_dnsv6_x=""
 	nvram commit
 	/sbin/restart_dhcpd
